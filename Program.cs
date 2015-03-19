d99c1842de46a01446a555c5120d3d88be390097
zominym <marc.zominy@wanadoo.fr>
2015-03-19 08:30:27 +0100

    Réparation des conneries

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupideVautour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b;
+            Console.WriteLine("istenen sayıyı sonuna .00 koyarak yaz");
+            while (!double.TryParse(Console.ReadLine(), out a))
+            {
+
+            }
+            b = a * a;
+            Console.WriteLine("Réponse : " + b);
+            Console.ReadLine();
+
+        }
+    }
+}
