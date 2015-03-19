97b294637b5aa532498cc9664084e610f98f287c
zominym <marc.zominy@wanadoo.fr>
2015-03-19 09:26:22 +0100

    Classes

---
+++ StupideVautour/Talon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupideVautour
+{
+    class Talon
+    {
+        List<CarteVS> cartes;
+        public Talon()
+        {
+            for (int i = -5; i <= -1; i++)
+            {
+                cartes.Add(new CarteVS(i));
+            }
+            for (int i = 1; i <=10 ; i++)
+            {
+                cartes.Add(new CarteVS(i));
+            }
+        }
+    }
+}
